0b2836a00344daac077a317da08c692d68bd1ecf
neomaxim <neomaximys@gmail.com>
2024-02-20 13:30:34 +0300

    lesson 54

---
+++ securityfull/src/main/java/auto/model/Auto.java
@@ -0,0 +1,97 @@
+package auto.model;
+
+import auto.entity.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "autos")
+@NamedEntityGraph(name="Auto.roles",attributeNodes = @NamedAttributeNode("roles"))
+public class Auto {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(name = "vin")
+    private String vin;
+
+
+    @Column(name = "year")
+    private int year;
+
+    @Column(name = "brand")
+    private String brand;
+
+    @Column(name = "status")
+    private boolean status;
+
+
+    @Column(name = "user")
+    private String user;
+@Column(name="avatar")
+    @JsonIgnore
+    private byte[] avatar;
+
+
+
+@OneToMany
+private List<Role> roles;
+
+ /*   public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+   *//* @Override
+    public String toString() {
+        return id + " " + vin + " " + year + " " + brand + " " + status;
+    }*//*
+
+    @Override
+    public String toString() {
+        return id + " " + vin + " " + year + " " + brand + " " + status+" "+ avatar;
+    }*/
+}
---
+++ securityfull/src/main/java/auto/service/impl/AutoServiceImpl.java
@@ -0,0 +1,59 @@
+package auto.service.impl;
+
+import auto.dto.AutoDto;
+import auto.exception.AutoNotFoundException;
+import auto.mapper.AutoMapper;
+import auto.model.Auto;
+import auto.repository.AutoRepository;
+import auto.service.AutoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+import java.util.List;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+@Service
+@RequiredArgsConstructor
+public class AutoServiceImpl implements AutoService {
+    private final AutoRepository autoRepository;
+    private final AutoMapper autoMapper;
+    @Override
+    public List<AutoDto> allCars() {
+        return autoRepository.findAll().stream().map(autoMapper::toAutoDto).toList();
+    }
+
+    @Override
+    public List<AutoDto> getAll(Pageable pageable) {
+
+       Page<Auto> all = autoRepository.findAll(pageable);
+         return autoMapper.toDtoList(all.getContent());
+    }
+    @Override
+    public List<AutoDto> findByVin(String vin) {
+        return autoMapper.toDtoList(autoRepository.findByVin(vin)); // поиск нужного нам vin
+
+    }
+
+    @Override
+    public AutoDto getById(Long id) {
+        return autoRepository.findById(id)
+                .map(autoMapper::toAutoDto)
+                .orElseThrow(AutoNotFoundException::new);
+    }
+
+
+@Override
+@Transactional
+public AutoDto save(AutoDto autoDto){
+    Auto auto = autoMapper.toAuto(autoDto);
+    autoRepository.save(auto);
+    return  autoMapper.toAutoDto(auto);
+}
+@Transactional
+    @Override
+    public void delete(Long id) {
+        autoRepository.deleteById(id);
+    }
+}
