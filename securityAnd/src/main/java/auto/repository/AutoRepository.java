ca18c89548474ae6fd9539d34dbbd873799be50b
neomaxim <neomaximys@gmail.com>
2024-02-20 12:53:30 +0300

    add 52

---
+++ securityAnd/src/main/java/auto/repository/AutoRepository.java
@@ -0,0 +1,14 @@
+package auto.repository;
+
+import auto.model.Auto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+public interface AutoRepository extends JpaRepository<Auto,Long> {
+
+  @Query(nativeQuery = true, value = "select * from autos where vin like :vin || '%'")
+    List<Auto> findByVin( String vin);
+
+
+
+}
